338ee898ffc76688ef784e3510db7d8b457d02c1
Jesús <jesusguerreropaniagua6@gmail.com>
2024-08-19 14:05:14 +0200

    feat(Auth-MS): :sparkles: integrated with auth-ms

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+
+import { NatsModule } from "src/transports/nats.module";
+
+import { AuthController } from "./auth.controller";
+
+@Module({
+  imports: [NatsModule],
+  controllers: [AuthController],
+})
+export class AuthModule {}
